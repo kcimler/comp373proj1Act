c5263c477fdc65f3c9e6615b66ccffc89aae06bf
Louis <elino@luc.edu>
2017-02-19 19:51:49 -0600

    Facility

--- src/FacilityDetail.java
+++ src/FacilityDetail.java
@@ -1,4 +1,41 @@
 
 public class FacilityDetail {
+	private String fAddress;
+	private int floorNumber;
+	private String fName;
+	private int facilityId;
+	private String buildingType;
+	        
 
+	public String getAddress() {
+		return fAddress;
+		}
+	public void setFAddress(String fAddress) {
+	    this.fAddress = fAddress;
+	    }
+	public int getFloorNumber() {
+		return floorNumber;
+	    }
+	public void setFloorNumber(int floorNumber) {
+	    this.floorNumber = floorNumber;
+	    }
+	public String getFName() {
+	    return fName;
+	    }
+	public void setFName(String fName) {
+	    this.fName = fName;
+	    }
+
+	public int getFacilityId() {
+	    return facilityId;
+	    }
+	public void setFacilityId(int facilityId) {
+	    this.facilityId = facilityId;
+	    }
+	public String getBuidingType() {
+	    return buildingType;
+	    }   
+	 public void setBuidingType(String buildingType) {
+	    this.buildingType = buildingType;
+	    }
 }
