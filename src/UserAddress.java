94dc92ec0756bed43d3c50a6e82ab1d1ede8f1a5
mac <mac@10.24.1.200>
2017-02-19 18:26:58 -0600

    UserAddress

---
+++ src/UserAddress.java
@@ -0,0 +1,47 @@
+
+public class UserAddress {
+	private String address;
+	private int unitNum;
+	private String city;
+	private String state;
+	private int zip;
+	private String country;
+	        
+
+	public String getAddress() {
+		return address;
+		}
+	public void setAddress(String address) {
+	    this.address = address;
+	    }
+	public int getUnitNum() {
+		return unitNum;
+	    }
+	public void setUnitNum(int unitNum) {
+	    this.unitNum = unitNum;
+	    }
+	public String getCity() {
+	    return city;
+	    }
+	public void setCity(String city) {
+	    this.city = city;
+	    }
+	public String getState() {
+	    return state;
+	    }
+	public void setState(String state) {
+	    this.state = state;
+	    }
+	public int getZip() {
+	    return zip;
+	    }
+	public void setZip(int zip) {
+	    this.zip = zip;
+	    }
+	public String getCountry() {
+	    return country;
+	    }   
+	 public void setCountry(String country) {
+	    this.country = country;
+	    }
+}
