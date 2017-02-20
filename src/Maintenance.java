a4aaed47ce12ee6c16071eefcdd9fe884ba605a1
DESKTOP-IOJT21N\kciml <kcimler@luc.edu>
2017-02-19 18:01:11 -0600

    Giving us a Starting Point

--- src/Maintenance.java
+++ src/Maintenance.java
@@ -2,4 +2,33 @@
  * Created by kciml on 2/19/2017.
  */
 public class Maintenance {
+    public void makeFacilityMaintRequest(){
+
+    }
+
+    public void scheduleMaintenance(){
+
+    }
+
+    public void calcMaintenanceCostForFacility(){
+
+    }
+
+    public void calcProblemRateForFacility(){
+
+    }
+
+    public void calcDownTimeForFacility(){
+
+    }
+
+    public void listMaintRequests(){
+
+    }
+    public void listMaintenance(){
+
+    }
+    public void listFacilityProblems(){
+
+    }
 }
