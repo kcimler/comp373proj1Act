54b58e28dc39589d6b9abfc0676fcca9ced31b11
Louis <elino@luc.edu>
2017-02-19 20:07:57 -0600

    FaciltyUse

--- src/FacilityUse.java
+++ src/FacilityUse.java
@@ -1,4 +1,24 @@
 
 public class FacilityUse {
+	
+	
+	public void isInUseDuringInterval() {
+		
+	}
+	public void assignFacilityToUse() {
+		
+	}
+	public void vacateFacility() {
+		
+	}
+	public void listInspections() {
+		
+	}
+	public void listActualUsage() {
+		
+	}
+	public void calcUsageRate() {
+		
+	}
 
 }
